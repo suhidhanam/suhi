2b55f7c2d742f5a68890226a43b8c34ac6ff7dea
suhidhanam <34937350+suhidhanam@users.noreply.github.com>
2019-01-11 11:56:22 +0530

    Create Name.java

---
+++ Name.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Name
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int a=sc.nextInt();
+        for(int i=0;i<a;i++)
+        {
+            System.out.println(s);
+        }
+    }
+}
